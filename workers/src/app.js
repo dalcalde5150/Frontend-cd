1b2a624880336d6020f03fe2ee277bbd4a53632d
CGR0 <carlos.garcs@uc.cl>
2022-10-02 17:13:42 -0300

    Separación backend y frontend

--- workers/src/app.js
+++
@@ -1,9 +0,0 @@
-const Koa = require('koa');
-const koaBody = require('koa-body');
-const app = new Koa();
-const port = 8080;
-
-app.use(koaBody());
-app.use(require('./routes').routes());
-
-app.listen(port, () => console.log(`Server started on port ${port}`));
--- workers/src/routes.js
+++
@@ -1,9 +0,0 @@
-const Router = require('koa-router');
-
-const router = new Router();
-
-router.use('/job', require('./routes/job').routes());
-router.use('/heartbeat', require('./routes/heartbeat').routes());
-
-
-module.exports = router;
