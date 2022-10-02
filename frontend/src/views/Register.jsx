f86b40284c2a094af11ef8cbcb59f4b0b85aeb91
luciano1 <you@example.com>
2022-10-02 18:13:11 -0300

    register

--- frontend/src/views/Register.jsx
+++ frontend/src/views/Register.jsx
@@ -2,6 +2,25 @@ import React from "react";
 
 export default function Register() {
     return (
-        <h2>Register</h2>
+        <div>
+            <h2>Register</h2>
+            <form action="<%= SubmitUsersPath %>" method="post">
+                <div>
+                    <label for="email">Email</label>
+                    <input type="text" name="email" />
+                </div>
+                <div>
+                    <label for="nickname">Nickname</label>
+                    <input type="text" name="nickname" />
+                </div>
+                <div>
+                    <label for="password">Password</label>
+                    <input type="text" name="password" />
+                </div>
+                <div>
+                    <input class="margin-title1" type="submit" name="Crear" value="CREAR" />
+                </div>
+            </form>
+        </div>  
     );
 }
