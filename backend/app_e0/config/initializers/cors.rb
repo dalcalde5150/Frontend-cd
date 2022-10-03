875d0884ff644c67746a9dcf73a5dfa5c32ad693
CGR0 <carlos.garcs@uc.cl>
2022-10-02 21:17:24 -0300

    Nueva configuracion cors

--- backend/app_e0/config/initializers/cors.rb
+++ backend/app_e0/config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'http://localhost:3002', 'https://e0carlosgarces.tk'
+      origins '*'
       resource '*', headers: :any, methods: [:get, :post]
     end
   end
