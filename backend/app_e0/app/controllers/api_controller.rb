74c758963237a2dc786e719f20c6c9ce11e3fae1
CGR0 <carlos.garcs@uc.cl>
2022-10-03 03:55:50 -0300

    Corrección pequeña

--- backend/app_e0/app/controllers/api_controller.rb
+++ backend/app_e0/app/controllers/api_controller.rb
@@ -15,8 +15,9 @@ class ApiController < ApplicationController
         if current_user?
             @job = Job.find_by(user_id: current_user, event_id: @id)
             if @job.blank? == false
-            @job.destroy
+                @job.destroy
             end
+        end
         @events = Event.order(id: :desc).limit(2000)
         @response = RestClient.post '44.208.40.132:8080/job', {'id' => @id, 'lat' => @lat, 'lon' => @lon, 'eventos' => @events}.to_json, :content_type => :json, :accept => :to_json
         @new = Job.create(user_id: current_user.id, event_id: @id, job_id: @response)
