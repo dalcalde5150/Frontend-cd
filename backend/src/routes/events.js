1f7b931ff723535947b822c9509086c47d5912aa
CGR0 <carlos.garcs@uc.cl>
2022-10-31 17:55:47 -0300

    Se arregla endpoint get eventos

--- backend/src/routes/events.js
+++ backend/src/routes/events.js
@@ -4,7 +4,13 @@ const router = new Router();
 router.get('get-events', '/:id', async (ctx) => {
     try {
         const events_list = await ctx.orm.Event.findAll({
-            include: {model: ctx.orm.Job, where: {id_user: ctx.params.id}}
+            include: {
+                model: ctx.orm.Job, 
+                where: {
+                    id_user: ctx.params.id
+                }, 
+                required: false
+            }
         });
         ctx.body = events_list;
         ctx.status = 200;
