06278b2bfa8e640acf1ff683f9d426758749b2db
dalcalde5150 <dalcalde@uc.cl>
2022-11-02 03:25:06 -0300

    appspec for codeDeploy

---
+++ scripts/application_stop.sh
@@ -0,0 +1,6 @@
+
+#!/bin/bash
+cd /home/ubuntu/proyecto-base-grupo-26
+
+echo "Stopping service... :)"
+sudo docker compose down
---
+++ scripts/validate_service.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /home/ubuntu/proyecto-base-grupo-26
+
+echo "Validating Service is Up... SKERE :)"
