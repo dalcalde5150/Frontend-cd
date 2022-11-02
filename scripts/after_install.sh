16ca794d5ceb195a645d3735ded3b46cad5b1c82
dalcalde5150 <dalcalde@uc.cl>
2022-11-02 04:02:06 -0300

    addd

---
+++ scripts/after_install.sh
@@ -0,0 +1,5 @@
+
+#!/bin/bash
+cd /home/ubuntu/proyecto-base-grupo-26
+
+echo "Running After Start service script... skere :)"
---
+++ scripts/application_start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd /home/ubuntu/proyecto-base-grupo-26
+
+echo "Starting Service... :)"
+sudo docker compose up -d --build
